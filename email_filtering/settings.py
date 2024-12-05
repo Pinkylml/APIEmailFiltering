d57703715db44cc37b64968e0b31af6c8f8b1cf2
Jefferson Cando <jeff_can1995@hotmail.es>
2024-12-04 19:51:17 -0500

    8 attemp in render

--- email_filtering/settings.py
+++ email_filtering/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['apiemailfiltering.onrender.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
